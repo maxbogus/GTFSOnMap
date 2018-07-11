6622463a19790ddac6c3cfb0a6a27a6e28418fce
Max Boguslavskiy <max.bogus@gmail.com>
2018-07-12 01:02:48 +0300

    added google map to project; replaced default data with map; fixed some errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public latitude: number;
+  public longitude: number;
+  public map: google.maps.Map;
+
+  @ViewChild('gmap') gmapElement: any;
+
+  ngOnInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+  setCenter(e: any) {
+    e.preventDefault();
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+  }
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId);
+  }
 }
